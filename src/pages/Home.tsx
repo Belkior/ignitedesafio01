39549f0872751c8f935bbea6b5fbda0241d69717
Belkior Alves <belkioralves@gmail.com>
2021-07-04 01:13:43 -0300

    Primeiro Desafio Ignite

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-
 import { Header } from '../components/Header';
 import { MyTasksList } from '../components/MyTasksList';
 import { TodoInput } from '../components/TodoInput';
@@ -10,19 +9,37 @@ interface Task {
   done: boolean;
 }
 
-export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+export function Home(task:Task) {
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task if it's not empty
+    if (newTaskTitle.trim() != '') {
+      const data = {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false,
+      };
+
+      setTasks([...tasks, data]);
+    }
   }
 
   function handleMarkTaskAsDone(id: number) {
-    //TODO - mark task as done if exists
+    const selectedTasks = tasks.map((task:Task) => {
+      const isTaskDone =
+      task.id === id && task.done
+          ? { ...task, done: false }
+          : task.id === id && !task.done
+          ? { ...task, done: true }
+          : { ...task };
+      return isTaskDone;
+    });
+
+    setTasks(selectedTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(tasks.filter((task:Task) => task.id !== id));
   }
 
   return (
@@ -31,11 +48,11 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <MyTasksList 
-        tasks={tasks} 
-        onPress={handleMarkTaskAsDone} 
-        onLongPress={handleRemoveTask} 
+      <MyTasksList
+        tasks={tasks}
+        onPress={handleMarkTaskAsDone}
+        onLongPress={handleRemoveTask}
       />
     </>
-  )
+  );
 }
